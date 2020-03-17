3ef8ab05e188305aad5308a809e0d482689d03ae
Karolína Vyskočilová <karolina@kybernaut.cz>
2020-03-17 14:15:55 +0100

    Add missing function

--- src/OptimizeWP.php
+++ src/OptimizeWP.php
@@ -108,8 +108,24 @@ class OptimizeWP
         remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
         remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );	
         remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-        add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+        add_filter( 'tiny_mce_plugins', array($this,'disable_emojis_tinymce') );
         add_filter( 'wp_resource_hints', array($this,'disable_emojis_remove_dns_prefetch'), 10, 2 );
+    }
+
+    
+    /**
+     * Filter function used to remove the tinymce emoji plugin.
+     * 
+     * @param    array  $plugins  
+     * @return   array              Difference betwen the two arrays
+     */
+    function disable_emojis_tinymce( $plugins ) {
+        if ( is_array( $plugins ) ) {
+            return array_diff( $plugins, array( 'wpemoji' ) );
+        }
+        
+        return array();
+
     }
 
     /**
